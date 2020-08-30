fa32cdbe9640e6098b74e16305ab692d987d2647
Isk3lo <iskelo.kap@gmail.com>
2020-08-30 12:30:42 +0200

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,37 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); 
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "15px 10px 0 10px";    
+    document.getElementById("logo").style.width = "192px";
+    document.getElementById("logo").style.height = "50px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(64,64,64)";
+    document.getElementById("menu").style.paddingTop = "10px";
+  } else {
+    document.getElementById("navbar").style.padding = "15px 10px";
+    document.getElementById("navbar").style.backgroundColor = "rgba(64,64,64,0)";
+    document.getElementById("logo").style.width = "231px";
+    document.getElementById("logo").style.height = "60px";
+    document.getElementById("menu").style.paddingTop = "15px";
+  }
+}
