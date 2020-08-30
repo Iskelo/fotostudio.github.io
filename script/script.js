45c48c151138b9f01bd6472810691746130ee53f
Isk3lo <iskelo.kap@gmail.com>
2020-08-30 13:14:30 +0200

    first page

--- script/script.js
+++ script/script.js
@@ -35,3 +35,21 @@ function scrollFunction() {
     document.getElementById("menu").style.paddingTop = "15px";
   }
 }
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
